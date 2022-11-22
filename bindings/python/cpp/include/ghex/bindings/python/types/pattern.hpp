d9203a456f5993a8d60df2de3f9bb7aab229b3d0
Stefano Ubbiali <subbiali@phys.ethz.ch>
2022-11-22 16:37:15 +0100

    Export __call__ for pattern class.

--- bindings/python/cpp/include/ghex/bindings/python/types/pattern.hpp
+++ bindings/python/cpp/include/ghex/bindings/python/types/pattern.hpp
@@ -18,6 +18,7 @@
 
 #include "ghex/structured/pattern.hpp"
 #include "ghex/structured/regular/domain_descriptor.hpp"
+#include "ghex/structured/regular/field_descriptor.hpp"
 #include "ghex/structured/regular/halo_generator.hpp"
 
 #include "ghex/bindings/python/type_list.hpp"
@@ -32,17 +33,22 @@ namespace python {
 namespace types {
 
 void pattern_container_exporter(py::module_& m) {
+    using architecture_type = typename gridtools::ghex::bindings::python::type_list::architecture_type;
     using context_type = typename gridtools::ghex::bindings::python::type_list::context_type;
+    using data_type = typename gridtools::ghex::bindings::python::type_list::data_type;
     using dim_type = typename gridtools::ghex::bindings::python::type_list::dim_type;
     using domain_id_type = typename gridtools::ghex::bindings::python::type_list::domain_id_type;
+    using layout_map_type = typename gridtools::ghex::bindings::python::type_list::layout_map_type;
 
     using domain_descriptor_type = gridtools::ghex::structured::regular::domain_descriptor<
         domain_id_type, dim_type>;
     using domain_range_type = typename gridtools::ghex::bindings::python::type_list::domain_range_type<domain_descriptor_type>;
+    using field_descriptor_type = gridtools::ghex::structured::regular::field_descriptor<
+        data_type, architecture_type, domain_descriptor_type, layout_map_type>;
     using halo_generator_type = gridtools::ghex::structured::regular::halo_generator<domain_id_type, dim_type>;
     using pattern_container_type = decltype(gridtools::ghex::make_pattern<
         gridtools::ghex::bindings::python::type_list::grid_type>(
-            std::declval<typename gridtools::ghex::bindings::python::type_list::context_type&>(),
+            std::declval<context_type&>(),
             std::declval<halo_generator_type&>(),
             std::declval<domain_range_type&>()
         )
@@ -58,7 +64,8 @@ void pattern_container_exporter(py::module_& m) {
         })
         .def_property_readonly_static("grid_type", [] (const pybind11::object&) {
             return gridtools::ghex::bindings::python::utils::demangle<typename pattern_container_type::grid_type>();
-        });
+        })
+        .def("__call__", &pattern_container_type::template operator()<field_descriptor_type>);
 
     auto make_pattern_wrapper = [] (context_type& context, halo_generator_type& hgen, domain_range_type& d_range) {
         return gridtools::ghex::make_pattern<gridtools::ghex::bindings::python::type_list::grid_type>(context, hgen, d_range);
