5d964b674ab78841040f89339201672edb9b6733
Stefano Ubbiali <subbiali@phys.ethz.ch>
2022-11-23 11:35:17 +0100

    Overload exchange().

--- bindings/python/cpp/include/ghex/bindings/python/types/communication_object.hpp
+++ bindings/python/cpp/include/ghex/bindings/python/types/communication_object.hpp
@@ -84,6 +84,121 @@ void communication_object_exporter(py::module_& m) {
                              buffer_info_type& b2,
                              buffer_info_type& b3) {
             return co.exchange(b1, b2, b3);
+        })
+        .def("exchange", [] (communication_object_type& co,
+                             buffer_info_type& b1,
+                             buffer_info_type& b2,
+                             buffer_info_type& b3,
+                             buffer_info_type& b4) {
+            return co.exchange(b1, b2, b3, b4);
+        })
+        .def("exchange", [] (communication_object_type& co,
+                             buffer_info_type& b1,
+                             buffer_info_type& b2,
+                             buffer_info_type& b3,
+                             buffer_info_type& b4,
+                             buffer_info_type& b5) {
+            return co.exchange(b1, b2, b3, b4, b5);
+        })
+        .def("exchange", [] (communication_object_type& co,
+                             buffer_info_type& b1,
+                             buffer_info_type& b2,
+                             buffer_info_type& b3,
+                             buffer_info_type& b4,
+                             buffer_info_type& b5,
+                             buffer_info_type& b6) {
+            return co.exchange(b1, b2, b3, b4, b5, b6);
+        })
+        .def("exchange", [] (communication_object_type& co,
+                             buffer_info_type& b1,
+                             buffer_info_type& b2,
+                             buffer_info_type& b3,
+                             buffer_info_type& b4,
+                             buffer_info_type& b5,
+                             buffer_info_type& b6,
+                             buffer_info_type& b7) {
+            return co.exchange(b1, b2, b3, b4, b5, b6, b7);
+        })
+        .def("exchange", [] (communication_object_type& co,
+                             buffer_info_type& b1,
+                             buffer_info_type& b2,
+                             buffer_info_type& b3,
+                             buffer_info_type& b4,
+                             buffer_info_type& b5,
+                             buffer_info_type& b6,
+                             buffer_info_type& b7,
+                             buffer_info_type& b8) {
+            return co.exchange(b1, b2, b3, b4, b5, b6, b7, b8);
+        })
+        .def("exchange", [] (communication_object_type& co,
+                             buffer_info_type& b1,
+                             buffer_info_type& b2,
+                             buffer_info_type& b3,
+                             buffer_info_type& b4,
+                             buffer_info_type& b5,
+                             buffer_info_type& b6,
+                             buffer_info_type& b7,
+                             buffer_info_type& b8,
+                             buffer_info_type& b9) {
+            return co.exchange(b1, b2, b3, b4, b5, b6, b7, b8, b9);
+        })
+        .def("exchange", [] (communication_object_type& co,
+                             buffer_info_type& b1,
+                             buffer_info_type& b2,
+                             buffer_info_type& b3,
+                             buffer_info_type& b4,
+                             buffer_info_type& b5,
+                             buffer_info_type& b6,
+                             buffer_info_type& b7,
+                             buffer_info_type& b8,
+                             buffer_info_type& b9,
+                             buffer_info_type& b10) {
+            return co.exchange(b1, b2, b3, b4, b5, b6, b7, b8, b9, b10);
+        })
+        .def("exchange", [] (communication_object_type& co,
+                             buffer_info_type& b1,
+                             buffer_info_type& b2,
+                             buffer_info_type& b3,
+                             buffer_info_type& b4,
+                             buffer_info_type& b5,
+                             buffer_info_type& b6,
+                             buffer_info_type& b7,
+                             buffer_info_type& b8,
+                             buffer_info_type& b9,
+                             buffer_info_type& b10,
+                             buffer_info_type& b11) {
+            return co.exchange(b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11);
+        })
+        .def("exchange", [] (communication_object_type& co,
+                             buffer_info_type& b1,
+                             buffer_info_type& b2,
+                             buffer_info_type& b3,
+                             buffer_info_type& b4,
+                             buffer_info_type& b5,
+                             buffer_info_type& b6,
+                             buffer_info_type& b7,
+                             buffer_info_type& b8,
+                             buffer_info_type& b9,
+                             buffer_info_type& b10,
+                             buffer_info_type& b11,
+                             buffer_info_type& b12) {
+            return co.exchange(b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12);
+        })
+        .def("exchange", [] (communication_object_type& co,
+                             buffer_info_type& b1,
+                             buffer_info_type& b2,
+                             buffer_info_type& b3,
+                             buffer_info_type& b4,
+                             buffer_info_type& b5,
+                             buffer_info_type& b6,
+                             buffer_info_type& b7,
+                             buffer_info_type& b8,
+                             buffer_info_type& b9,
+                             buffer_info_type& b10,
+                             buffer_info_type& b11,
+                             buffer_info_type& b12,
+                             buffer_info_type& b13) {
+            return co.exchange(b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13);
         });
 
     using communication_handle_type = typename communication_object_type::handle_type;
