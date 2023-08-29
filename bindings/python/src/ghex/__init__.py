55d6e897856f43870d1afdabe5f5983793e15fa5
stubbiali <subbiali@phys.ethz.ch>
2023-08-29 23:46:06 +0200

    Simplify __init__.

--- bindings/python/src/ghex/__init__.py
+++ bindings/python/src/ghex/__init__.py
@@ -10,10 +10,7 @@
 __copyright__ = "Copyright (c) 2014-2021 ETH Zurich"
 __license__ = "BSD-3-Clause"
 
-from ghex.utils.setup import update_path, validate_library_version
-
-update_path()
-
 import ghex_py_bindings as _ghex
+from ghex.utils.setup import validate_library_version
 
 validate_library_version(_ghex)
--- bindings/python/src/ghex/utils/setup.py
+++ bindings/python/src/ghex/utils/setup.py
@@ -7,39 +7,12 @@
 #
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
-import os
-import sys
+from mpi4py import MPI
 import warnings
-
-try:
-    import mpi4py
-except ImportError:
-    mpi4py = None
-
-
-def update_path():
-    ghex_py_lib_path = os.environ.get("GHEX_PY_LIB_PATH", None)
-
-    if ghex_py_lib_path is None:
-        current_dir = os.path.dirname(os.path.relpath(__file__))
-        project_root = os.path.abspath(os.path.join(current_dir, "../../../../.."))
-        ghex_py_lib_path = os.path.join(project_root, "build")
-    if not os.path.exists(ghex_py_lib_path):
-        raise ValueError(
-            f"The build directory `{ghex_py_lib_path}` does not exist.\n"
-            f"Please set the correct build directory via the env variable GHEX_PY_LIB_PATH."
-        )
-
-    sys.path.append(ghex_py_lib_path)
 
 
 def validate_library_version(_ghex):
     """Check MPI library version string of MPI4Py and bindings match."""
-    if mpi4py is None:
-        return
-
-    from mpi4py import MPI
-
     ghex_mpi_lib_ver = _ghex.utils.mpi_library_version()
     mpi4py_lib_ver = MPI.Get_library_version()
     # fix erroneous nullbyte at the end
