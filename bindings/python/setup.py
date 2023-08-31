692710c5a30b4ee5103e05d25fd18a720b920704
stubbiali <subbiali@phys.ethz.ch>
2023-08-31 10:33:07 +0200

    Refine setup.py.

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -2,7 +2,6 @@
 import os
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
-import shutil
 import subprocess
 
 # build dependencies
@@ -26,11 +25,6 @@ class CMakeBuild(build_ext):
         build_dir = os.path.abspath(os.path.join(this_dir, "_build"))
         pybind11_dir = pybind11.get_cmake_dir()
         mpi4py_dir = os.path.abspath(os.path.join(mpi4py.get_include(), ".."))
-
-        ghex_use_gpu = os.environ.get("GHEX_USE_GPU", "False")
-        ghex_gpu_type = os.environ.get("GHEX_GPU_TYPE", "AUTO")
-        ghex_gpu_arch = os.environ.get("GHEX_GPU_ARCH", "")
-
         cmake_args = [
             f"-B{build_dir}",
             "-DCMAKE_BUILD_TYPE=Release",
@@ -38,10 +32,15 @@ class CMakeBuild(build_ext):
             "-DHAVE_MPI4PY=True",
             f"-Dpybind11_DIR={pybind11_dir}",
             f"-DPY_MPI4PY={mpi4py_dir}",
-            f"-DUSE_GPU={ghex_use_gpu}",
-            f"-DGHEX_GPU_TYPE={ghex_gpu_type}",
-            f"-DGHEX_GPU_ARCH={ghex_gpu_arch}",
         ]
+
+        ghex_use_gpu = os.environ.get("GHEX_USE_GPU", "False")
+        cmake_args.append(f"-DUSE_GPU={ghex_use_gpu}")
+        if bool(ghex_use_gpu):
+            ghex_gpu_type = os.environ.get("GHEX_GPU_TYPE", "AUTO")
+            ghex_gpu_arch = os.environ.get("GHEX_GPU_ARCH", "")
+            cmake_args += [f"-DGHEX_GPU_TYPE={ghex_gpu_type}", f"-DGHEX_GPU_ARCH={ghex_gpu_arch}"]
+
         subprocess.run(["cmake", source_dir, *cmake_args], capture_output=False)
         subprocess.run(["cmake", "--build", build_dir, "--", "-j 8"], capture_output=False)
 
@@ -49,8 +48,6 @@ class CMakeBuild(build_ext):
         src_path = os.path.join(build_dir, self.get_ext_filename(ext_name))
         trg_path = self.get_ext_fullpath(ext_name)
         self.copy_file(src_path, trg_path)
-
-        # shutil.rmtree(build_dir)
 
 
 if __name__ == "__main__":
